13b1ca700fbc82041a5fe5b967a2b3e80fa015bc
Vicente <carvalho.vicente0207@gmail.com>
2025-03-29 23:52:17 -0300

    Add sombra nos textos

---
+++ script.js
@@ -0,0 +1,33 @@
+// FOTO-INTRO
+const foto = document.getElementById("foto1")
+const circle = document.getElementById("circle")
+
+foto.addEventListener("mouseenter", () => { //Quando o mouse entrar, a sombra aumenta
+    circle.classList.add("aumentar-sombra")
+})
+
+foto.addEventListener("mouseleave", () => { //Quando o mouse sair, a sombra volta
+    circle.classList.remove("aumentar-sombra")
+})
+
+// FOTO-BIO
+const foto2 = document.getElementById("foto2")
+
+foto2.addEventListener("mouseenter", () => {
+    foto2.classList.add("aumentar-sombra")
+})
+
+foto2.addEventListener("mouseleave", () => {
+    foto2.classList.remove("aumentar-sombra")
+})
+
+// TEXTOS
+const text = document.getElementById("textGlow")
+
+text.addEventListener("mouseenter", () => {
+    text.classList.add("aumentar-sombra")
+})
+
+text.addEventListener("mouseleave", () => {
+    text.classList.remove("aumentar-sombra")
+})
